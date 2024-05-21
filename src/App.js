4c27d1daf8c323ab830eac5161005a1a2db707ae
Gireesh <gireesh.shukla@echttech.com>
2024-05-21 12:53:30 +0530

    pre changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router , Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import Master from './components/Partial/Master';
 import Header from './components/Layout/Header/Header';
 import Dashboard from './pages/Dashboard/Dashboard';
@@ -8,17 +8,16 @@ import Ecce_details from './pages/ECCE/Ecce_details';
 
 function App() {
   return (
-    <Router>
-      <Header/>
-       <Routes>
-         <Route exact path="/" element={<Master/>} />            
-         <Route exact path="*" element={<Master/>} />
-         <Route exact path="dashboard" element={<Dashboard/>} />
-         <Route exact path="ecce" element={<Ecce/>} />
-         <Route exact path="ecce_details" element={<Ecce_details/>} />
-       </Routes>
-    </Router>
-    
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Master />} />
+        <Route exact path="*" element={<Master />} />
+        <Route exact path="dashboard" element={<Dashboard />} />
+        <Route exact path="ecce" element={<Ecce />} />
+        <Route exact path="ecce_details" element={<Ecce_details />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
